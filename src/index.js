7379cbed84e2a6228c56adcba2c08e374c3d0315
Dylan-Gresham <dgresh724@gmail.com>
2023-09-13 11:31:01 -0600

    Fix import statement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Imports
 import css from './index.css'
-import {initialize} from './initialize';
+import {initialize} from './initialize.js';
 
 // Code
 const contentDiv = document.createElement('div');
